83bc0b843ec49ec79310c18d6e364ac657d99f7c
FreddyWordingham <freddy@digilab.com>
2022-08-23 00:16:30 +0100

    Added get_all and add_new routes

--- api/app/api/main.py
+++ api/app/api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 from .database import DB
 from .settings import ENV
@@ -9,4 +9,18 @@ app = FastAPI()
 
 @app.get("/")
 async def homepage():
+    r = DB["collection"].insert_one({"test": "beans"})
+    print(f">>>> {r.inserted_id}")
+
     return ENV.MAIN_DATABASE
+
+
+@app.get("/db/{collection}")
+async def get_all(collection: str):
+    return DB[collection].find()
+
+
+@app.post("/db/{collection}")
+async def add_new(request: Request, collection: str):
+    record = await request.json()
+    return DB[collection].insert_one(record)
